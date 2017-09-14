f7b4b911350e901afed0101fb4a35646d1be2a2d
Gitu Mbugua <gmbugua38@gmail.com>
2017-09-14 16:28:17 +0300

    add template for adding new food

---
+++ app/new-food.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FoodComponent } from './food.component';
+
+@Component({
+  selector: "new-food",
+  template: `
+  
+  `
+})
+
+export class NewFoodComponent {
+  
+}

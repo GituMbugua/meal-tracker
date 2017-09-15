44d27b014ab3f7a7d70767e94ee9475262bc2abd
Gitu Mbugua <gmbugua38@gmail.com>
2017-09-15 15:46:40 +0300

    add component to add meals to the list

--- app/new-food.component.js
+++ app/new-food.component.js
@@ -1,24 +1,35 @@
-// import { Component } from '@angular/core';
-// import { Food } from './food.component';
-// @Component({
-//   selector: "new-food",
-//   template: `
-//   <div class="container">
-//   <div>
-//     <label>Enter a meal</label>
-//     <input #newMeal>
-//   </div>
-//   <div>
-//     <label >Enter details</label>
-//     <input #newDetails>
-//   </div>  
-//   <div>
-//     <label >Number of calories</label>
-//     <input #newCalories>
-//   </div>
-// </div>
-//   `
-// })
-// export class NewFoodComponent {
-// } 
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var food_model_1 = require('./food.model');
+var NewFoodComponent = (function () {
+    function NewFoodComponent() {
+        this.newMealSender = new core_1.EventEmitter;
+    }
+    NewFoodComponent.prototype.newClicked = function (name, details, calories) {
+        var toAdd = new food_model_1.Food(name, details, calories);
+        this.newMealSender.emit(toAdd);
+    };
+    __decorate([
+        core_1.Output(), 
+        __metadata('design:type', Object)
+    ], NewFoodComponent.prototype, "newMealSender", void 0);
+    NewFoodComponent = __decorate([
+        core_1.Component({
+            selector: "new-food",
+            template: "\n  <div class=\"container\">\n    <h1>New Meal</h1>\n    <div>\n      <label>Enter a meal</label>\n      <input #newMeal>\n    </div>\n    <div>\n      <label>Enter details</label>\n      <input #newDetails>\n    </div>  \n    <div>\n      <label>Number of calories</label>\n      <input #newCalories>\n    </div>\n    <button (click)=\"newClicked(newMeal.value, newDetails.value, newCalories.value)\">Add</button>\n  </div>\n  "
+        }), 
+        __metadata('design:paramtypes', [])
+    ], NewFoodComponent);
+    return NewFoodComponent;
+}());
+exports.NewFoodComponent = NewFoodComponent;
 //# sourceMappingURL=new-food.component.js.map
--- app/new-food.component.ts
+++ app/new-food.component.ts
@@ -1,27 +1,32 @@
-// import { Component } from '@angular/core';
-// import { Food } from './food.component';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Food } from './food.model';
 
-// @Component({
-//   selector: "new-food",
-//   template: `
-//   <div class="container">
-//   <div>
-//     <label>Enter a meal</label>
-//     <input #newMeal>
-//   </div>
-//   <div>
-//     <label >Enter details</label>
-//     <input #newDetails>
-//   </div>  
-//   <div>
-//     <label >Number of calories</label>
-//     <input #newCalories>
-//   </div>
-// </div>
-//   `
-// })
+@Component({
+  selector: "new-food",
+  template: `
+  <div class="container">
+    <h1>New Meal</h1>
+    <div>
+      <label>Enter a meal</label>
+      <input #newMeal>
+    </div>
+    <div>
+      <label>Enter details</label>
+      <input #newDetails>
+    </div>  
+    <div>
+      <label>Number of calories</label>
+      <input #newCalories>
+    </div>
+    <button (click)="newClicked(newMeal.value, newDetails.value, newCalories.value)">Add</button>
+  </div>
+  `
+})
 
-// export class NewFoodComponent {
-
-  
-// }
+export class NewFoodComponent {
+  @Output() newMealSender = new EventEmitter;
+  newClicked(name: string, details: string, calories: number) {
+    var toAdd: Food = new Food(name, details, calories);
+    this.newMealSender.emit(toAdd);
+  }  
+}

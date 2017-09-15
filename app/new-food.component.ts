fc9a536c18d604e59b7c67822fc474dac86f7281
Gitu Mbugua <gmbugua38@gmail.com>
2017-09-15 12:17:37 +0300

    commented out code

--- app/new-food.component.ts
+++ app/new-food.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { FoodComponent } from './food.component';
+// import { Component } from '@angular/core';
+// import { Food } from './food.component';
 
-@Component({
-  selector: "new-food",
-  template: `
-  
-  `
-})
+// @Component({
+//   selector: "new-food",
+//   template: `
+//   <div class="container">
+//   <div>
+//     <label>Enter a meal</label>
+//     <input #newMeal>
+//   </div>
+//   <div>
+//     <label >Enter details</label>
+//     <input #newDetails>
+//   </div>  
+//   <div>
+//     <label >Number of calories</label>
+//     <input #newCalories>
+//   </div>
+// </div>
+//   `
+// })
 
-export class NewFoodComponent {
+// export class NewFoodComponent {
+
   
-}
+// }
